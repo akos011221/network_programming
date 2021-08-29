7342ac0920e4bec1d3bbee53fbb4748b12652982
bajkevero-majom <57492846+bajkevero-majom@users.noreply.github.com>
2021-08-29 14:56:52 +0200

    Update bgpcheck.py

--- bgpcheck.py
+++ bgpcheck.py
@@ -8,28 +8,26 @@ n = len(sys.argv)
 for i in range(1, n):
     cmargs.append(sys.argv[i])
 
-
 # device parameters, connecting
 device_type = 'cisco_ios'
 username = 'te445587teadm'
 password = getpass()
 verbose = True
 device = cmargs[0]
-print('-'*20+'\nConnecting to '+device+'\n'+'-'*20)
-cisco_dev = {'device_type': 'cisco_ios','ip': device,'username': username,'password': password}
+print('-' * 20 + '\nConnecting to ' + device + '\n' + '-' * 20)
+cisco_dev = {'device_type': 'cisco_ios', 'ip': device, 'username': username, 'password': password}
 session = ConnectHandler(**cisco_dev)
 out = session.send_command_timing('enable')
 
-
 # bgp summary-check
 if 'bgp' or 'BGP' in cmargs:
-    print('Checking BGP...')
+    print('-=Checking BGP=-')
 
     # checking whether BGP is active on the device..
     out = session.send_command('show bgp ipv4 unicast summary')
     # if not active..
     if '% BGP not active' in out:
-        print('BGP is not configured on '+device)
+        print('BGP is not configured on ' + device)
 
     # if active..
     else:
@@ -41,32 +39,33 @@ if 'bgp' or 'BGP' in cmargs:
             pieces = i.split()
             # if peer is Active or Idle and not in Idle (Admin)
             # if pieces' length is larger than 9, then there's an '(Admin)' word after Idle
-    
-            if pieces[9] == 'Idle' or 'Active' and len(pieces) <= 10:
-                out = session.send_command_timing(
-                    'show bgp ipv4 unicast neighbors ' + pieces[0] + ' | include Description')
-                check = out.split(
-                    ' ')  # checking if there's a description for the peer
-                if len(check) > 1:
-                    # print with description
-                    print('BGP Peer ' + pieces[0] + ' "' + out.replace(' Description: ', '') + '" has been down for ' +
-                          pieces[8] + ' on ' + device)
+
+            out = session.send_command_timing(
+                'show bgp ipv4 unicast neighbors ' + pieces[0] + ' | include Description')
+            check = out.split(' ')  # checking if there's a description for the peer
+            desc_str = ''
+
+            if len(check) > 1:
+                desc_str = out
+            else:
+                desc_str = 'Description: N/A'
+
+            # if connection is down
+            if pieces[9] == 'Active' or pieces[9] == 'Idle':
+                if len(pieces) <= 10:
+
+                    print('BGP Peer ' + pieces[0] + ' has been DOWN for ' +
+                          pieces[8] + ' on ' + device + ' # ' + desc_str)
+
                 else:
-                    # print w/o description
-                    print('BGP Peer ' + pieces[0] + ' has been down for ' + pieces[8] + ' on ' + device)
+                    print('BGP Peer ' + pieces[
+                        0] + ' is manually SHUT DOWN [Idle (Admin)] on ' + device + ' # ' + desc_str)
 
-            elif len(pieces) > 9:  # if the 'pieces' list is larger than 9, the connection is in Idle (Admin) state
+            # if connection is up
+            else:
+                print('BGP Peer ' + pieces[0] + ' is is UP for ' + pieces[8] + ' on ' + device + ' # ' + desc_str)
 
-                out = session.send_command_timing(
-                    'show bgp ipv4 unicast neighbors ' + pieces[0] + ' | include Description')
-                check = out.split(' ')  # same description check as above..
-
-                if len(check) > 1:
-                    # print with description
-                    print('BGP Peer ' + pieces[0] + ' "' + out.replace('Description: ',
-                                                                       '') + '" is manually shut down [Idle (Admin)] on ' + device)
-                else:
-                    # print w/o description
-                    print('BGP Peer ' + pieces[0] + ' is manually shut down [Idle (Admin)] on ' + device)
-                    
         out = ''
+
+out = session.send_command_timing('show clock')
+print('-' * 20 + '\nSystem clock: ' + out)
