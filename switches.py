364c8a581d71ec77e4f5e5e433680e2290d9b08f
akos011221 <57492846+akos011221@users.noreply.github.com>
2022-06-13 03:34:29 +0200

    Update switches.py

--- switches.py
+++ switches.py
@@ -1,12 +1,15 @@
+## finds the "Down" interfaces on a list of switches and prints the date since they are down.
+
 from getpass import getpass
 from netmiko import ConnectHandler
 import os
 import re
+import csv
 
 password = getpass()
 switches = []
 
-with open('hostnames') as f:
+with open('switches-list') as f:
     lines = f.readlines()
     for line in lines:
         switches.append(line)
@@ -14,10 +17,11 @@ with open('hostnames') as f:
 for dev in switches:
 
     verbose = True
+    # print('Connecting to ' + dev)
     cisco_dev = {'device_type': 'cisco_ios', 'ip': dev, 'username': os.getlogin(), 'password': password}
     try:
         session = ConnectHandler(**cisco_dev, fast_cli=True)
-        
+
         print("<--------------------------------------->")
         print(dev.strip().center(40))
         print("<--------------------------------------->")
@@ -32,19 +36,22 @@ for dev in switches:
           if "Gi" in line and "notconnect" in line:
             intf = line.split()[0]
             interfaces.append(intf)
-        
+
         for inf in interfaces:
-        
+          if "Gi" in inf:
+            inf_num = inf.replace("Gi","")
+          if "Te" in inf:
+            inf_num = inf.replace("Te","")
           out = session.send_command(f"show int {inf} | in line")
           print(out.replace("is down, line protocol is ", ""))
-          out = session.send_command(f"show log | in {inf2}.*down")
+          out = session.send_command(f"show log | in {inf_num}.*down")
           lines = out.split("\n")
           date = lines[-1].partition("%")[0]
           if not date:
-            print("DOWN SINCE --> No info, probably it was never up")
+            print("No info found.")
           else:
-            print("DOWN SINCE --> ", date)
-          
+            print("Status is down since\t", date)
+
           print()
 
     except Exception as e:
